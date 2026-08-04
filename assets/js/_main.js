e622331b1ef162270c676f4260d218ec6939fa02
Ana Sofia Inacio <ana.carpinteiroinacio@physics.ox.ac.uk>
2026-08-04 21:23:16 +0100

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -22,15 +22,14 @@ function determineComputedTheme() {
   if (themeSetting != "system") {
     return themeSetting;
   }
-  return browserPref ? "dark" : "light";
+  return "dark";
 }
 
 // Set the theme on page load or when explicitly called
-function setTheme(theme) {
-  const use_theme = theme ||
-    localStorage.getItem("theme") ||
-    $("html").attr("data-theme") ||
-    browserPref;
+const use_theme = theme ||
+  localStorage.getItem("theme") ||
+  $("html").attr("data-theme") ||
+  "dark";
 
   if (use_theme === "dark") {
     $("html").attr("data-theme", "dark");
